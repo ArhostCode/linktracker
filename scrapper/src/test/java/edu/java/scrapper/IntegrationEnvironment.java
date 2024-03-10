61ccd9b43ffad82cd4397b8131a07c4496985c6a
ArhostCode <e-gulyakin@mail.ru>
2024-03-10 20:48:56 +0300

    Using deprecated Liquibase#update

--- scrapper/src/test/java/edu/java/scrapper/IntegrationEnvironment.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationEnvironment.java
@@ -2,14 +2,6 @@ package edu.java.scrapper;
 
 import java.nio.file.Path;
 import liquibase.Liquibase;
-import liquibase.UpdateSummaryEnum;
-import liquibase.UpdateSummaryOutputEnum;
-import liquibase.command.CommandScope;
-import liquibase.command.core.UpdateCommandStep;
-import liquibase.command.core.helpers.ChangeExecListenerCommandStep;
-import liquibase.command.core.helpers.DatabaseChangelogCommandStep;
-import liquibase.command.core.helpers.DbUrlConnectionCommandStep;
-import liquibase.command.core.helpers.ShowSummaryArgument;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
@@ -44,28 +36,7 @@ public abstract class IntegrationEnvironment {
             new DirectoryResourceAccessor(Path.of("migrations")),
             database
         );
-        updateCommand(liquibase).execute();
-    }
-
-    @SneakyThrows
-    private static CommandScope updateCommand(Liquibase liquibase) {
-        CommandScope updateCommand = new CommandScope(UpdateCommandStep.COMMAND_NAME);
-        updateCommand.addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, liquibase.getDatabase());
-        updateCommand.addArgumentValue(UpdateCommandStep.CHANGELOG_ARG, liquibase.getDatabaseChangeLog());
-        updateCommand.addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, liquibase.getChangeLogFile());
-        updateCommand.addArgumentValue(UpdateCommandStep.CONTEXTS_ARG, "");
-        updateCommand.addArgumentValue(UpdateCommandStep.LABEL_FILTER_ARG, "");
-        updateCommand.addArgumentValue(
-            ChangeExecListenerCommandStep.CHANGE_EXEC_LISTENER_ARG,
-            liquibase.getDefaultChangeExecListener()
-        );
-        updateCommand.addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG);
-        updateCommand.addArgumentValue(
-            DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS,
-            liquibase.getChangeLogParameters()
-        );
-        updateCommand.addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY, UpdateSummaryEnum.VERBOSE);
-        return updateCommand;
+        liquibase.update();
     }
 
     @DynamicPropertySource
