8a5b02a7b6ed0f947426b2e8bc7a3fee4d6d8a1f
ArhostCode <e-gulyakin@mail.ru>
2024-04-06 17:07:24 +0300

    Simple way to create dlq

--- bot/src/main/java/edu/java/bot/kafka/KafkaUpdatesListener.java
+++ bot/src/main/java/edu/java/bot/kafka/KafkaUpdatesListener.java
@@ -1,26 +1,21 @@
 package edu.java.bot.kafka;
 
-import edu.java.bot.configuration.ApplicationConfig;
 import edu.java.bot.dto.request.LinkUpdate;
 import edu.java.bot.service.LinkNotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class KafkaUpdatesListener {
     private final LinkNotificationService linkNotificationService;
-    private final KafkaTemplate<String, LinkUpdate> kafkaTemplate;
-    private final ApplicationConfig config;
 
     @KafkaListener(topics = "${app.kafka.updates-topic-name}", groupId = "bot")
+    @RetryableTopic(attempts = "1", dltStrategy = DltStrategy.FAIL_ON_ERROR, dltTopicSuffix = "_dlq")
     public void listenUpdate(LinkUpdate update) {
-        try {
-            linkNotificationService.notifyLinkUpdate(update);
-        } catch (Exception e) {
-            kafkaTemplate.send(config.kafka().updatesTopicName() + "_dlq", update);
-        }
+        linkNotificationService.notifyLinkUpdate(update);
     }
 }
